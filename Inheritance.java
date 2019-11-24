3d8274893400a19a4fb3eb7e6b36c691c3d9a1ac
vbyelo <56196047+vbyelo@users.noreply.github.com>
2019-11-24 10:27:57 +0100

    Update Inheritance.java

--- Inheritance.java
+++ Inheritance.java
@@ -1,21 +1,25 @@
 
-
-
 public class Inheritance {
     public static void main(String[] args) {
         Pracownik pracownik = new Pracownik();
         pracownik.imie = "Grzegorz";
         pracownik.nazwisko = "Kowalski";
-        System.out.println(pracownik.imie + " " + pracownik.nazwisko + " "
-                + (pracownik.pelnyEtap ? "pracuje na pelny etap" : "pracuje na pół etapu") + ", jego pansja " + pracownik.Pensja());
+        pracownik.Wydrukowac();
+        // System.out.println(pracownik.imie + " " + pracownik.nazwisko + " "
+        // + (pracownik.pelnyEtap ? "pracuje na pelny etap" : "pracuje na pół etapu") +
+        // ", jego pansja " + pracownik.Pensja());
 
-        Pracownik pracownik2 = new Pracownik();
-        pracownik2.imie = "Halina";
-        pracownik2.nazwisko = "Kowalczyk";
-        System.out.println(pracownik2.imie + " " + pracownik2.nazwisko + " "
-                + (pracownik2.pelnyEtap ? "pracuje na pelny etap" : "pracuje na pół etapu") + ", jej pansja " + pracownik2.Pensja());
-        
+        Pracownik pracownik2 = new Pracownik("Halina", "Kowalczyk", Plec.Kobieta);
+        pracownik2.Wydrukowac();
+        // System.out.println(pracownik2.imie + " " + pracownik2.nazwisko + " "
+        // + (pracownik2.pelnyEtap ? "pracuje na pelny etap" : "pracuje na pół etapu") +
+        // ", jej pansja " + pracownik2.Pensja());
+
     }
+}
+
+enum Plec {
+    Mezczyzna, Kobieta
 }
 
 class Pracownik {
@@ -24,6 +28,7 @@ class Pracownik {
     float wyplaty;
     int dniWTygodniu;
     boolean pelnyEtap;
+    Plec plec;
 
     public Pracownik() {
         this.imie = "";
@@ -31,12 +36,29 @@ class Pracownik {
         this.wyplaty = 18.0f;
         this.dniWTygodniu = 5;
         this.pelnyEtap = true;
+        this.plec = Plec.Mezczyzna;
     }
 
-    public float Pensja(){
+    public Pracownik(String imie, String nazwisko, Plec plec) {
+        this.imie = imie;
+        this.nazwisko = nazwisko;
+        this.wyplaty = 18.0f;
+        this.dniWTygodniu = 5;
+        this.pelnyEtap = true;
+        this.plec = plec;
+    }
+
+    public float Pensja() {
         int dni = this.dniWTygodniu * 4;
-        int godziny = this.pelnyEtap ? 8 :4; 
-        return this.wyplaty * dni *godziny; 
+        int godziny = this.pelnyEtap ? 8 : 4;
+        return this.wyplaty * dni * godziny;
+    }
+
+    public void Wydrukowac() {
+        System.out.println(this.imie + " " + this.nazwisko + " "
+                + (this.pelnyEtap ? "pracuje na pelny etap" : "pracuje na pół etapu")
+                + (this.plec == Plec.Mezczyzna ? ", jego pansja " : ", jej pansja ") + this.Pensja());
+
     }
 
 }
