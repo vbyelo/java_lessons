fc27e1c6570915367d99d7866165dfdb21440b74
vbyelo <56196047+vbyelo@users.noreply.github.com>
2019-11-24 09:44:58 +0100

    Update Inheritance.java

--- Inheritance.java
+++ Inheritance.java
@@ -1,24 +1,42 @@
 
+
+
 public class Inheritance {
-    public static void main(String[] args){
-        Pracownik pracownik= new Pracownik();
-        System.out.println(pracownik.name + " " + pracownik.nazwisko + " " + (pracownik.pelnyEtap? "pracuje na pelny etap": "pracuje na pół etapu"));
+    public static void main(String[] args) {
+        Pracownik pracownik = new Pracownik();
+        pracownik.imie = "Grzegorz";
+        pracownik.nazwisko = "Kowalski";
+        System.out.println(pracownik.imie + " " + pracownik.nazwisko + " "
+                + (pracownik.pelnyEtap ? "pracuje na pelny etap" : "pracuje na pół etapu") + ", jego pansja " + pracownik.Pensja());
+
+        Pracownik pracownik2 = new Pracownik();
+        pracownik2.imie = "Halina";
+        pracownik2.nazwisko = "Kowalczyk";
+        System.out.println(pracownik2.imie + " " + pracownik2.nazwisko + " "
+                + (pracownik2.pelnyEtap ? "pracuje na pelny etap" : "pracuje na pół etapu") + ", jej pansja " + pracownik2.Pensja());
+        
     }
 }
 
-class Pracownik{
-    String name;
+class Pracownik {
+    String imie;
     String nazwisko;
     float wyplaty;
     int dniWTygodniu;
     boolean pelnyEtap;
 
-    public Pracownik(){
-        this.name = "";
+    public Pracownik() {
+        this.imie = "";
         this.nazwisko = "";
         this.wyplaty = 18.0f;
         this.dniWTygodniu = 5;
         this.pelnyEtap = true;
+    }
+
+    public float Pensja(){
+        int dni = this.dniWTygodniu * 4;
+        int godziny = this.pelnyEtap ? 8 :4; 
+        return this.wyplaty * dni *godziny; 
     }
 
 }
